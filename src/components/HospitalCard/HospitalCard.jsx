5aa7961b7ffb1e892b0d906ec439aa34545b384d
Vikas Tiwari <30732434+ervikasti@users.noreply.github.com>
2024-06-20 09:16:11 +0000

    Changes Fixed

--- src/components/HospitalCard/HospitalCard.jsx
+++ src/components/HospitalCard/HospitalCard.jsx
@@ -1,43 +1,61 @@
-import { useState } from 'react';
-import BookingForm from '../BookingForm/BookingForm';
-import styles from './HospitalCard.module.css';
+import { useState } from "react";
+import BookingForm from "../BookingForm/BookingForm";
+import styles from "./HospitalCard.module.css";
 
-const HospitalCard = ({hospitalList}) => {
+const HospitalCard = ({ hospitalList }) => {
+  console.log(hospitalList);
 
-    console.log(hospitalList);
-
-    const [bookingFormEnabled,setBookingFormEnabled] = useState(false);
-
-
-    return(
-        <>
-            <div>{hospitalList.length} medical center avalilable in {hospitalList[0].City}</div>
-            { hospitalList.map( (data)=> (
-                <div>
-                    <div key={data['Provider ID']}className={styles.container}>
-                        <div><img src='/images/hospital_animated.svg' alt='logo'/></div>
-                        <div className={styles.container_details}>
-                            <h3>{data['Hospital Name']}</h3>
-                            <h5>{data['City']},{data['State']}</h5>
-                            <p>{data['Hospital Type']}</p>
-                            <p>Free Consultation</p>
-                            <div>Rating {data['Hospital overall rating']}</div>
-                        </div>
-                        <div>
-                            <div>Status</div> 
-                            <button onClick={()=>{setBookingFormEnabled(!bookingFormEnabled)}}>Book a free center visit</button>
-                        </div>
-                    </div>
-                    { bookingFormEnabled && (<BookingForm></BookingForm>)}
-                </div>
-                ))
-            }
-
-           
-        </>
-        
-        
+  const Details = ({ key, name, city, state, type, rating }) => {
+    const [bookingFormIsActive, setBookingFormIsActive] = useState(false);
+    return (
+      <div key={key}>
+        <div className={styles.container}>
+          <div>
+            <img src="/images/hospital_animated.svg" alt="logo" />
+          </div>
+          <div className={styles.container_details}>
+            <h3>{name}</h3>
+            <h5>
+              {city},{state}
+            </h5>
+            <p>{type}</p>
+            <p>Free Consultation</p>
+            <div>Rating {rating}</div>
+          </div>
+          <div>
+            <div>Status</div>
+            <button
+              onClick={() => {
+                setBookingFormIsActive(!bookingFormIsActive);
+              }}
+            >
+              Book a free center visit
+            </button>
+          </div>
+        </div>
+        {bookingFormIsActive && <BookingForm></BookingForm>}
+      </div>
     );
-}
+  };
 
-export {HospitalCard};
+  return (
+    <>
+      <div>
+        {hospitalList.length} medical center avalilable in{" "}
+        {/* {hospitalList[0].City} */}
+      </div>
+      {hospitalList.map((data) => (
+        <Details
+          key={data["Provider ID"]}
+          name={data["Hospital Name"]}
+          city={data["City"]}
+          state={data["State"]}
+          type={data["Hospital Type"]}
+          rating={data["Hospital overall rating"]}
+        ></Details>
+      ))}
+    </>
+  );
+};
+
+export { HospitalCard };
